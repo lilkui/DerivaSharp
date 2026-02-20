1b8fa219a27bae5f8397957742356dab977d9294
LiLkUi <jiangping.li@outlook.com>
2026-02-21 01:20:16 +0800

    Add NullCalendar

---
+++ src/Time/NullCalendar.cs
@@ -0,0 +1,15 @@
+﻿namespace DerivaSharp.Time;
+
+/// <summary>
+///     A calendar in which every day is a trading day.
+/// </summary>
+public sealed class NullCalendar : ICalendar
+{
+    /// <inheritdoc />
+    public int TradingDaysPerYear => 365;
+
+    /// <summary>
+    ///     Always returns <see langword="true" />; all dates are considered trading days.
+    /// </summary>
+    public bool IsTradingDay(DateOnly date) => true;
+}
