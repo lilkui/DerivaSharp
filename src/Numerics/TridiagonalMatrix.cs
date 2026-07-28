f800f83b6e211b703869199aad7970fff348711c
LiLkUi <jiangping.li@outlook.com>
2026-07-28 17:32:45 +0800

    perf(numerics): tighten TridiagonalMatrix validation and SIMD

--- src/Numerics/TridiagonalMatrix.cs
+++ src/Numerics/TridiagonalMatrix.cs
@@ -7,56 +7,81 @@ namespace DerivaSharp.Numerics;
 /// <summary>
 ///     Represents a tridiagonal matrix and provides operations for solving linear systems.
 /// </summary>
-/// <param name="lower">The lower diagonal elements.</param>
-/// <param name="main">The main diagonal elements.</param>
-/// <param name="upper">The upper diagonal elements.</param>
-public sealed class TridiagonalMatrix(double[] lower, double[] main, double[] upper)
+public sealed class TridiagonalMatrix
 {
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="TridiagonalMatrix"/> class.
+    /// </summary>
+    /// <param name="lower">The lower diagonal elements.</param>
+    /// <param name="main">The main diagonal elements.</param>
+    /// <param name="upper">The upper diagonal elements.</param>
+    /// <exception cref="ArgumentNullException">A diagonal is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentException">The diagonals do not have the same length.</exception>
+    public TridiagonalMatrix(double[] lower, double[] main, double[] upper)
+    {
+        ArgumentNullException.ThrowIfNull(lower);
+        ArgumentNullException.ThrowIfNull(main);
+        ArgumentNullException.ThrowIfNull(upper);
+
+        if (lower.Length != main.Length || upper.Length != main.Length)
+        {
+            ThrowHelper.ThrowArgumentException(ExceptionMessages.SpanLengthsMustMatch);
+        }
+
+        Lower = lower;
+        Main = main;
+        Upper = upper;
+    }
+
     /// <summary>
     ///     Gets the lower diagonal elements of the matrix.
     /// </summary>
     /// <value>An array of lower diagonal elements.</value>
-    public double[] Lower { get; } = lower;
+    public double[] Lower { get; }
 
     /// <summary>
     ///     Gets the main diagonal elements of the matrix.
     /// </summary>
     /// <value>An array of main diagonal elements.</value>
-    public double[] Main { get; } = main;
+    public double[] Main { get; }
 
     /// <summary>
     ///     Gets the upper diagonal elements of the matrix.
     /// </summary>
     /// <value>An array of upper diagonal elements.</value>
-    public double[] Upper { get; } = upper;
+    public double[] Upper { get; }
 
     /// <summary>
     ///     Multiplies the tridiagonal matrix by a vector.
     /// </summary>
     /// <param name="vector">The vector to multiply.</param>
     /// <param name="result">The span to store the result.</param>
+    /// <exception cref="ArgumentException">The vector and result do not match the matrix size.</exception>
     public void Multiply(ReadOnlySpan<double> vector, Span<double> result)
     {
         int n = Main.Length;
+
+        if (vector.Length != n || result.Length != n)
+        {
+            ThrowHelper.ThrowArgumentException(ExceptionMessages.SpanLengthsMustMatch);
+        }
 
         if (n == 0)
         {
             return;
         }
 
-        if (Lower.Length != n || Upper.Length != n || vector.Length != n || result.Length != n)
+        ReadOnlySpan<double> lower = Lower;
+        ReadOnlySpan<double> main = Main;
+        ReadOnlySpan<double> upper = Upper;
+
+        if (n == 1)
         {
-            ThrowHelper.ThrowArgumentException(ExceptionMessages.SpanLengthsMustMatch);
+            result[0] = main[0] * vector[0];
+            return;
         }
 
-        if (n > 1)
-        {
-            result[0] = Main[0] * vector[0] + Upper[0] * vector[1];
-        }
-        else
-        {
-            result[0] = Main[0] * vector[0];
-        }
+        result[0] = main[0] * vector[0] + upper[0] * vector[1];
 
         int i = 1;
         int interiorLength = n - 2;
@@ -66,27 +91,25 @@ public sealed class TridiagonalMatrix(double[] lower, double[] main, double[] up
             int simdLength = interiorLength - interiorLength % simdWidth;
             for (; i < 1 + simdLength; i += simdWidth)
             {
-                Vector<double> lowerVector = new(Lower.AsSpan(i, simdWidth));
-                Vector<double> mainVector = new(Main.AsSpan(i, simdWidth));
-                Vector<double> upperVector = new(Upper.AsSpan(i, simdWidth));
+                Vector<double> lowerVector = new(lower.Slice(i, simdWidth));
+                Vector<double> mainVector = new(main.Slice(i, simdWidth));
+                Vector<double> upperVector = new(upper.Slice(i, simdWidth));
                 Vector<double> previousVector = new(vector.Slice(i - 1, simdWidth));
                 Vector<double> currentVector = new(vector.Slice(i, simdWidth));
                 Vector<double> nextVector = new(vector.Slice(i + 1, simdWidth));
 
-                Vector<double> resultVector = lowerVector * previousVector + mainVector * currentVector + upperVector * nextVector;
+                Vector<double> resultVector = Vector.FusedMultiplyAdd(lowerVector, previousVector, mainVector * currentVector);
+                resultVector = Vector.FusedMultiplyAdd(upperVector, nextVector, resultVector);
                 resultVector.CopyTo(result.Slice(i, simdWidth));
             }
         }
 
         for (; i < n - 1; i++)
         {
-            result[i] = Lower[i] * vector[i - 1] + Main[i] * vector[i] + Upper[i] * vector[i + 1];
+            result[i] = lower[i] * vector[i - 1] + main[i] * vector[i] + upper[i] * vector[i + 1];
         }
 
-        if (n > 1)
-        {
-            result[n - 1] = Lower[n - 1] * vector[n - 2] + Main[n - 1] * vector[n - 1];
-        }
+        result[n - 1] = lower[n - 1] * vector[n - 2] + main[n - 1] * vector[n - 1];
     }
 
     /// <summary>
@@ -94,13 +117,25 @@ public sealed class TridiagonalMatrix(double[] lower, double[] main, double[] up
     /// </summary>
     /// <param name="rhs">The right-hand side vector b.</param>
     /// <param name="result">The span to store the solution vector x.</param>
+    /// <exception cref="ArgumentException">The right-hand side and result do not match the matrix size.</exception>
     public void Solve(ReadOnlySpan<double> rhs, Span<double> result)
     {
         int n = Main.Length;
 
-        if (Lower.Length != n || Upper.Length != n || rhs.Length != n || result.Length != n)
+        if (rhs.Length != n || result.Length != n)
         {
             ThrowHelper.ThrowArgumentException(ExceptionMessages.SpanLengthsMustMatch);
+        }
+
+        if (n == 0)
+        {
+            return;
+        }
+
+        if (n == 1)
+        {
+            result[0] = rhs[0] / Main[0];
+            return;
         }
 
         using SpanOwner<double> upperPrimeBuffer = SpanOwner<double>.Allocate(n);
@@ -109,7 +144,6 @@ public sealed class TridiagonalMatrix(double[] lower, double[] main, double[] up
         Span<double> upperPrime = upperPrimeBuffer.Span;
         Span<double> rhsPrime = rhsPrimeBuffer.Span;
 
-        // Forward elimination
         upperPrime[0] = Upper[0] / Main[0];
         rhsPrime[0] = rhs[0] / Main[0];
 
@@ -120,11 +154,9 @@ public sealed class TridiagonalMatrix(double[] lower, double[] main, double[] up
             rhsPrime[i] = (rhs[i] - Lower[i] * rhsPrime[i - 1]) / denominator;
         }
 
-        // Last row
-        rhsPrime[n - 1] = (rhs[n - 1] - Lower[n - 1] * rhsPrime[n - 2]) / (Main[n - 1] - Lower[n - 1] * upperPrime[n - 2]);
-
-        // Backward substitution
-        result[n - 1] = rhsPrime[n - 1];
+        int last = n - 1;
+        rhsPrime[last] = (rhs[last] - Lower[last] * rhsPrime[last - 1]) / (Main[last] - Lower[last] * upperPrime[last - 1]);
+        result[last] = rhsPrime[last];
 
         for (int i = n - 2; i >= 0; i--)
         {
--- tests/Numerics/TridiagonalMatrixTest.cs
+++ tests/Numerics/TridiagonalMatrixTest.cs
@@ -7,6 +7,31 @@ namespace DerivaSharp.Tests;
 public sealed class TridiagonalMatrixTest
 {
     [Fact]
+    public void Constructor_WithDifferentDiagonalLengths_ThrowsArgumentException()
+    {
+        Assert.Throws<ArgumentException>(() => new TridiagonalMatrix([0.0], [2.0, 2.0], [0.0, 0.0]));
+    }
+
+    [Fact]
+    public void Multiply_WithEmptyMatrix_ReturnsEmptyResult()
+    {
+        TridiagonalMatrix matrix = new([], [], []);
+
+        matrix.Multiply(ReadOnlySpan<double>.Empty, Span<double>.Empty);
+    }
+
+    [Fact]
+    public void Multiply_WithSingleElement_ReturnsExpectedValue()
+    {
+        TridiagonalMatrix matrix = new([0.0], [2.0], [0.0]);
+        double[] result = new double[1];
+
+        matrix.Multiply([3.0], result);
+
+        Assert.Equal(6.0, result[0]);
+    }
+
+    [Fact]
     public void Multiply_WithSimdAndScalarRemainder_ReturnsExpectedValues()
     {
         int n = Vector<double>.Count + 3;
@@ -46,4 +71,67 @@ public sealed class TridiagonalMatrixTest
             Assert.Equal(expected[i], actual[i], 12);
         }
     }
+
+    [Fact]
+    public void Solve_WithEmptyMatrix_ReturnsEmptyResult()
+    {
+        TridiagonalMatrix matrix = new([], [], []);
+
+        matrix.Solve(ReadOnlySpan<double>.Empty, Span<double>.Empty);
+    }
+
+    [Fact]
+    public void Solve_WithSingleRow_ReturnsExpectedValue()
+    {
+        TridiagonalMatrix matrix = new([0.0], [2.0], [0.0]);
+        double[] result = new double[1];
+
+        matrix.Solve([6.0], result);
+
+        Assert.Equal(3.0, result[0]);
+    }
+
+    [Fact]
+    public void Solve_WithSeparateSpans_ReturnsExpectedValues()
+    {
+        TridiagonalMatrix matrix = CreateThreeRowMatrix();
+        double[] result = new double[3];
+
+        matrix.Solve([1.0, 0.0, 1.0], result);
+
+        Assert.Equal(1.0, result[0], 12);
+        Assert.Equal(1.0, result[1], 12);
+        Assert.Equal(1.0, result[2], 12);
+    }
+
+    [Fact]
+    public void Solve_InPlace_ReturnsExpectedValues()
+    {
+        TridiagonalMatrix matrix = CreateThreeRowMatrix();
+        double[] rhsAndResult = [1.0, 0.0, 1.0];
+
+        matrix.Solve(rhsAndResult, rhsAndResult);
+
+        Assert.Equal(1.0, rhsAndResult[0], 12);
+        Assert.Equal(1.0, rhsAndResult[1], 12);
+        Assert.Equal(1.0, rhsAndResult[2], 12);
+    }
+
+    [Fact]
+    public void Solve_WithPartiallyOverlappingSpans_ReturnsExpectedValues()
+    {
+        TridiagonalMatrix matrix = CreateThreeRowMatrix();
+        double[] storage = [1.0, 0.0, 1.0, 0.0];
+
+        matrix.Solve(storage.AsSpan(0, 3), storage.AsSpan(1, 3));
+
+        Assert.Equal(1.0, storage[1], 12);
+        Assert.Equal(1.0, storage[2], 12);
+        Assert.Equal(1.0, storage[3], 12);
+    }
+
+    private static TridiagonalMatrix CreateThreeRowMatrix() => new(
+        [0.0, -1.0, -1.0],
+        [2.0, 2.0, 2.0],
+        [-1.0, -1.0, 0.0]);
 }
